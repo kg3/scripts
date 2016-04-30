5553ecdd093eb034c071afff59ca8b7fd86928e5
Kurt <kg3@users.noreply.github.com>
2016-04-30 11:18:52 -0500

    Create fuck_samba.sh

---
+++ fuck_samba.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# fuck samba
+# http://serverfault.com/questions/294109/why-doesnt-sshfs-let-me-look-into-a-mounted-directory
+# for mac
+RUSER=user
+SERVER=ser.ver.add
+PORT=2222
+
+DIR1=/dir1/
+DIR2=/dir2/
+
+OPTIONS='-o allow_other -o kernel_cache -o auto_cache -o reconnect -o compression=no -o cache_timeout=600 -o ServerAliveInterval=15'
+
+sshfs $OPTIONS $RUSER@$SERVER:$DIR1 $DIR2
