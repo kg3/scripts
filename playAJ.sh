738f181d0febcc3e1d28c702abbdcd29ef42f868
Kurt <kg3@users.noreply.github.com>
2016-04-30 11:21:26 -0500

    Create playAJ.sh

---
+++ playAJ.sh
@@ -0,0 +1,3 @@
+#/bin/bash
+# Stream AlJazeera Live
+ffplay -i "rtmp://aljazeeraflashlivefs.fplive.net:443/aljazeeraflashlive-live?videoId=883816736001&lineUpId=&pubId=665003303001&playerId=751182905001&affiliateId=/aljazeera_eng_med?videoId=883816736001&lineUpId=&pubId=665003303001&playerId=751182905001&affiliateId= live=true"
