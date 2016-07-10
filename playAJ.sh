66acda76f5507496977bf66e0395deca82bd7c83
Kurt <kg3@users.noreply.github.com>
2016-07-10 16:24:42 -0500

    Delete playAJ.sh

--- playAJ.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-# Stream AlJazeera Live
-ffplay -i "rtmp://aljazeeraflashlivefs.fplive.net:443/aljazeeraflashlive-live?videoId=883816736001&lineUpId=&pubId=665003303001&playerId=751182905001&affiliateId=/aljazeera_eng_med?videoId=883816736001&lineUpId=&pubId=665003303001&playerId=751182905001&affiliateId= live=true"
-
-exit 0
