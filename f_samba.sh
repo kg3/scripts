0202d0c7075d49b7f955c7392d5155f7cbf7a8c8
Kurt <kg3@users.noreply.github.com>
2016-07-26 20:05:59 -0500

    Update f_samba.sh

--- f_samba.sh
+++ f_samba.sh
@@ -2,7 +2,6 @@
 # f!ck samba; for real after how long I've been using it and it never quite works correctly
 # mostly the problem is timing out or populating a directory on another machine
 # http://serverfault.com/questions/294109/why-doesnt-sshfs-let-me-look-into-a-mounted-directory
-# for mac
 RUSER=user
 SERVER=ser.ver.add
 PORT=2222
@@ -10,7 +9,7 @@ PORT=2222
 DIR1=/dir1/
 DIR2=/dir2/
 
-OPTIONS='-o allow_other -o kernel_cache -o auto_cache -o reconnect -o compression=no -o cache_timeout=600 -o ServerAliveInterval=15'
+OPTIONS='-p $PORT -o allow_other -o kernel_cache -o auto_cache -o reconnect -o compression=no -o cache_timeout=600 -o ServerAliveInterval=15'
 
 sshfs $OPTIONS $RUSER@$SERVER:$DIR1 $DIR2
 
